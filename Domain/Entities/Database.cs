57cef16a75f82004dfd0a90012da7ae0c4c01dae
AngreeDruw <andrei.412395@gmail.com>
2023-03-25 16:38:46 +0700

    Added enity DatabaseRelationship

--- Domain/Entities/Database.cs
+++ Domain/Entities/Database.cs
@@ -8,18 +8,18 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    enum DatabaseTypes
+    public enum DatabaseTypes
     {
         MSSql
     }
 
     public class Database : EntityBase
     {
-        string Name { get; set; }
+        public string Name { get; set; }
         public string Description { get; set; }
-        string ConnactionString { get; set; }
-        DatabaseTypes DatabaseType { get; set; }
-        Solution Solution { get; set; }
-        Guid SolutionId { get; set; }
+        public string ConnactionString { get; set; }
+        public DatabaseTypes DatabaseType { get; set; }
+        public Solution Solution { get; set; }
+        public Guid SolutionId { get; set; }
     }
 }
---
+++ Domain/Entities/DatabaseRelationship.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class DatabaseRelationship : EntityBase
+    {
+        public Database Leading { get; set; }
+        public Guid LeadingId { get; set; }
+        public Database Slave { get; set; }
+        public Guid SlaveId { get; set; }
+    }
+}
