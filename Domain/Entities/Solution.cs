0d3ee2bbdf45b468e9337328b9bd72c3a1a07125
AngreeDruw <andrei.412395@gmail.com>
2023-03-25 16:27:35 +0700

    Добавьте файлы проекта.

---
+++ Domain/Entities/Solution.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities.Base;
+
+namespace Domain.Entities
+{
+    public class Solution : EntityBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
