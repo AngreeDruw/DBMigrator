ce36a89e95dd35ba7d9c091a6df911cf399dd895
AngreeDruw <andrei.412395@gmail.com>
2023-03-25 16:33:32 +0700

    Added entity Database

---
+++ Domain/Entities/Database.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    enum DatabaseTypes
+    {
+        MSSql
+    }
+
+    public class Database : EntityBase
+    {
+        string Name { get; set; }
+        public string Description { get; set; }
+        string ConnactionString { get; set; }
+        DatabaseTypes DatabaseType { get; set; }
+        Solution Solution { get; set; }
+        Guid SolutionId { get; set; }
+    }
+}
