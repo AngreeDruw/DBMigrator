517beeb2f4e26eace84d3cb53f81168eb05b7f9c
AngreeDruw <andrei.412395@gmail.com>
2023-03-25 16:51:20 +0700

    Added entity Action

---
+++ Domain/Entities/Action.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities.Base;
+
+namespace Domain.Entities
+{
+    public class Action : EntityBase
+    {
+        public string Name { get; set; }
+        public int SortNumber { get; set; } = 0;
+        public DatabaseRelationship Relationship { get; set; }
+        public Guid RelationshipId { get; set; }
+        public string ActionJSON { get; set; }
+    }
+}
